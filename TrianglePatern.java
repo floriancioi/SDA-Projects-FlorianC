f0aed8487101655d7985838dce79c8c580140bae
Florian <florincioi123@gmail.com>
2019-05-13 12:52:01 +0300

    Primul commit

---
+++ TrianglePatern.java
@@ -0,0 +1,22 @@
+package main.java.Exercises;
+
+import java.util.Scanner;
+
+public class TrianglePatern {
+    public static void main(String[] args) {
+        int i, j, n;
+        n = 10;
+
+        for (i = 1; i <= n; i++) {
+            for (j = 1; j <= i; j++)
+                System.out.print(j);
+
+            System.out.println("");
+
+
+        }
+
+    }
+}
+
+
