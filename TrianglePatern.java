c73dc97d32b970ee6ebe09f6407e7f382855d743
Florian <florincioi123@gmail.com>
2019-05-13 13:01:58 +0300

    Aplicatie Triangle

---
+++ TrianglePatern.java
@@ -0,0 +1,19 @@
+package main.java.Exercises;
+
+import java.util.Scanner;
+
+public class TrianglePatern {
+    public static void main(String[] args) {
+        int i, j;
+
+        for (i = 1; i <= 10; i++) {
+            for (j = 1; j <= i; j++)
+                System.out.print(j);
+
+            System.out.println("");
+
+        }
+    }
+}
+
+
